eb63abcf7d69d8f7f7b67fa168532e847843e6ed
cabrerojas <gcabrerojas@gmail.com>
2021-04-25 20:31:06 -0400

    rutas
    Se implementa Lazyload en las rutas y se genera menú de navegación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-   
+    path: 'template',
+    loadChildren: () => import('./template/template.module').then( m => m.TemplateModule )
   },
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.module').then( m => m.ReactiveModule )
+  },
+  {
+    path: '**',
+    redirectTo: 'template'
+  }
 ];
 
 @NgModule({
