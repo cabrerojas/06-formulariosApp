9cf3d284ce894dd6c6fd4769d2bd656a0f6e0e6e
cabrerojas <gcabrerojas@gmail.com>
2021-04-25 22:34:19 -0400

    Directiva min

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar(): void {
+    console.log(this.miFormulario.value);
+
+  }
+
+  nombreValido(): boolean {
+    return this.miFormulario?.controls.producto?.invalid
+          && this.miFormulario?.controls.producto?.touched;
+  }
+
+  precioValido(): boolean {
+    return this.miFormulario?.controls.precio?.value < 0
+          && this.miFormulario?.controls.precio?.touched;
   }
 
 }
